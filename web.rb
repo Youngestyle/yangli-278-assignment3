fa53d37d4948d7a326f5bc55d2ab05c527bdaf9a
Youngestyle <Youngestyle@users.noreply.github.com>
2016-11-15 18:17:08 -0800

    Add files via upload

--- web.rb
+++ web.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
 require 'slim'
 require 'sass'
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/students.db")
+
+class Student
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :description, Text
+  property :height, Integer
+  property :dob, Date
+  
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
+end
+
+DataMapper.finalize
 
 configure do
 	enable :sessions
@@ -41,6 +60,51 @@ end
 get '/logout' do
 	session.clear
 	redirect to ('/login')
+end
+
+
+
+get '/students' do
+  halt slim :login unless session[:admin]
+  @students = Student.all
+  slim :students
+end
+
+get '/students/new' do
+  halt slim :login unless session[:admin]
+  @student = Student.new
+  slim :new_student
+end
+
+get '/students/:id' do
+  halt slim :login unless session[:admin]
+  @student = Student.get(params[:id])
+  slim :show_student
+end
+
+get '/students/:id/edit' do
+  halt slim :login unless session[:admin]
+  @student = Student.get(params[:id])
+  slim :edit_student
+end
+
+post '/students' do  
+  halt slim :login unless session[:admin]
+  student = Student.create(params[:student])
+  redirect to("/students/#{student.id}")
+end
+
+put '/students/:id' do
+  halt slim :login unless session[:admin]
+  student = Student.get(params[:id])
+  student.update(params[:student])
+  redirect to("/students/#{student.id}")
+end
+
+delete '/students/:id' do
+  halt slim :login unless session[:admin]
+  Student.get(params[:id]).destroy
+  redirect to('/students')
 end
 
 
