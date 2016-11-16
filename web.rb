1696f8bb4d58e2a36578e43cc49b1ac62be1bab3
Youngestyle <Youngestyle@users.noreply.github.com>
2016-11-15 18:18:57 -0800

    Add files via upload

--- web.rb
+++ web.rb
@@ -64,49 +64,6 @@ end
 
 
 
-get '/students' do
-  halt slim :login unless session[:admin]
-  @students = Student.all
-  slim :students
-end
-
-get '/students/new' do
-  halt slim :login unless session[:admin]
-  @student = Student.new
-  slim :new_student
-end
-
-get '/students/:id' do
-  halt slim :login unless session[:admin]
-  @student = Student.get(params[:id])
-  slim :show_student
-end
-
-get '/students/:id/edit' do
-  halt slim :login unless session[:admin]
-  @student = Student.get(params[:id])
-  slim :edit_student
-end
-
-post '/students' do  
-  halt slim :login unless session[:admin]
-  student = Student.create(params[:student])
-  redirect to("/students/#{student.id}")
-end
-
-put '/students/:id' do
-  halt slim :login unless session[:admin]
-  student = Student.get(params[:id])
-  student.update(params[:student])
-  redirect to("/students/#{student.id}")
-end
-
-delete '/students/:id' do
-  halt slim :login unless session[:admin]
-  Student.get(params[:id]).destroy
-  redirect to('/students')
-end
-
 
 not_found do
   slim :not_found
